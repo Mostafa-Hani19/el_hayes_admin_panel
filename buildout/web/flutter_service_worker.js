e089cef99756a7b0e64d471c84107cfddbf5409c
Mostafa Hani <mostafa.197.hani@gmail.com>
2025-07-08 16:02:40 +0300

    Added web build to docs for GitHub Pages

--- buildout/web/flutter_service_worker.js
+++ buildout/web/flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "406a78416118d038696245b3f42f85a2
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "ee286292a473d92610af59646447616f",
+"flutter_bootstrap.js": "f4835d1e898f2042eeccdcc0a996f559",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
